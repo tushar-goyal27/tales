67c87f7dfb3865e942cae9d368fa19f978122137
Tushar Goyal <tushar.lifetg@gmail.com>
2022-11-27 11:36:14 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Histogram from './components/Histogram';
 
 function App() {
+  const [freqData, setFreqData] = useState([[0], [0]]);
+  const cleanData = (txt) => {
+    txt = txt.replace(/[(),.@_:/"?!><;\-\n]/g, ' ');
+    var words = txt.toLowerCase().split(' ');
+    return words
+  }
+
+  const onSubmitBtn = (e) => {
+    e.preventDefault();
+
+    fetch("https://www.terriblytinytales.com/test.txt")
+    .then(response => response.text())
+    .then(data => {
+      var words = cleanData(data);
+      var wordMap = {};
+      words.forEach((w) => {
+          if (!wordMap[w]) {
+              wordMap[w] = 0;
+          }
+          wordMap[w] += 1;
+      });
+      var top20 = Object.entries(wordMap)
+                        .sort((a, b) => b[1] - a[1])
+                        .slice(1, 21);
+
+      var keys = top20.map(i => i[0]);
+      var vals = top20.map(i => i[1]);
+
+      setFreqData([keys, vals]);
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button className="mainbtn" type="submit" onClick={(e) => {onSubmitBtn(e)}}>Submit</button>
+      <Histogram freqData={freqData} />
     </div>
   );
 }
---
+++ src/components/Histogram.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef, useState } from 'react';
+// import './App.css';
+
+const Histogram = (props) => {
+    const [labels, setLabels] = useState(props.freqData[0]);
+    const [data, setData] = useState(props.freqData[1]);
+
+    const mainRef = useRef(null);
+
+    const vals = []
+    for(let i = 1; i < 29; i++) {
+        vals.push(i);
+    }
+
+    useEffect(() => {
+        console.log(data);
+        if(props.freqData[0][0] == 0) {
+            mainRef.current.style.display = "none";
+        } else {
+            setData(props.freqData[1]);
+            setLabels(props.freqData[0]);
+            mainRef.current.style.display = "flex";
+        }
+        
+    }, [props.freqData]);
+
+    return (
+        <div className='histogram' ref={mainRef}>
+            <div className='vals'>
+                {vals.map(ele => {
+                    return (
+                        ele % 2 !== 0 ?
+                        <div className='num'>
+                            {ele}
+                        </div> :
+                        <div className='num'>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className='col-flex'>
+                <div className='main-data'>
+                    {
+                        data.map(ele => {
+                            return (
+                                <div className='bar' style={{height: (ele*15) + "px"}}>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className='labels'>
+                    {
+                        labels.map(ele => {
+                            return(
+                                <div className='label'>
+                                    {ele}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Histogram;
